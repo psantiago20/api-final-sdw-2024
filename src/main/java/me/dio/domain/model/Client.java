00f9f9520dc9be7113cfd11c967d18402cde090b
psantiago20 <pedroasanti@yahoo.com.br>
2024-06-10 17:37:16 -0300

    Implementação da camada de dominio

---
+++ src/main/java/me/dio/domain/model/Client.java
@@ -0,0 +1,73 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 14)
+    private String phone;
+
+    @Column(length = 11)
+    private String cpf;
+
+    @Column(length = 8)
+    private String zip_code;
+
+    private String country;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getZip_code() {
+        return zip_code;
+    }
+
+    public void setZip_code(String zip_code) {
+        this.zip_code = zip_code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
